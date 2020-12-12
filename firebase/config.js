65cdc783a53e9295bc2cdf1d5222d3ccb64070a7
Namdev0411 <nams2bxlien@gmail.com>
2020-12-12 20:24:48 +0900

    Initialize project using Create React App

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+const firebaseConfig = {
+    apiKey: "AIzaSyAlUwRIVXvcTJ4-IFKT38uX1XQEvRJbR7c",
+    authDomain: "photo-gallery-94c5d.firebaseapp.com",
+    projectId: "photo-gallery-94c5d",
+    storageBucket: "photo-gallery-94c5d.appspot.com",
+    messagingSenderId: "321692831414",
+    appId: "1:321692831414:web:c6983cf5dc71694916a017"
+  };
+  firebase.initializeApp(firebaseConfig);
